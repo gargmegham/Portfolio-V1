941767e608153c119186f850eb7dcbc76a464b94
Megham-Garg <33658001+Megham-Garg@users.noreply.github.com>
2021-05-15 18:22:03 +0530

    Update theme.js

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -54,8 +54,8 @@ const theme = {
   hamAfter: `bottom 0.1s ease-in 0.25s, transform 0.22s cubic-bezier(0.55, 0.055, 0.675, 0.19)`,
   hamAfterActive: `bottom 0.1s ease-out, transform 0.22s cubic-bezier(0.215, 0.61, 0.355, 1) 0.12s`,
 
-  navDelay: 1000,
-  loaderDelay: 2000,
+  navDelay: 800,
+  loaderDelay: 1400,
 };
 
 export default theme;
