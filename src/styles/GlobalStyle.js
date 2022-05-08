b7c2b0bb546cbbb2f26dc673ca7a0a122464e3e9
Megham Garg <gargmegham3@gmail.com>
2022-05-08 11:13:57 +0530

    added font-display as swap
    https://web.dev/font-display/?utm_source=lighthouse&utm_medium=devtools

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -33,6 +33,7 @@ const GlobalStyle = createGlobalStyle`
     line-height: 1.3;
     font-family: ${fonts.Calibre};
     font-size: ${fontSizes.xl};
+    font-display: swap;
     ${media.phablet`font-size: ${fontSizes.lg};`}
 
     &.hidden {
